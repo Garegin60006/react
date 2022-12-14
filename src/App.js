696c354c6c6ad7911b2e0e31b38be25bac33b548
Garegin <garegin.joinunlock@gmail.com>
2022-12-14 15:42:09 +0400

    second react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
+import { useState } from 'react';
 
 import Bye from './Bye';
 
+// const zn = useState("");
+// const value = zn[0];
+// const setValue = zn[1];
+
+
 
 function App() {
+  const [val, setVal] = useState(null);
   return (
-    <div>
-      <div>App component</div>
-      <Bye />
-      <button onClick={(evt) => alert(123)}>Click Me</button>
+    <div className='App'>
+      <h1>{val}</h1>
+      <input type="text" value={val} onChange={(e) => {
+        setVal(e.target.value);
+      }} />
     </div>
   );
 }
