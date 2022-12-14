f580b97b82c450c0681bbd6e125b8a9cd18fc8e0
Garegin <garegin.joinunlock@gmail.com>
2022-12-14 16:20:59 +0400

    decrease of codes, useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import { useState } from 'react';
-
-import Bye from './Bye';
-
-// const zn = useState("");
-// const value = zn[0];
-// const setValue = zn[1];
+import './App.css';
+import LessText from './LessText';
 
 
 
 function App() {
-  const [val, setVal] = useState(null);
   return (
     <div className='App'>
-      <h1>{val}</h1>
-      <input type="text" value={val} onChange={(e) => {
-        setVal(e.target.value);
-      }} />
+      <LessText
+        text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim iste obcaecati suntLorem, ipsum dolor sit amet consectetur adipisicing elit. Enim iste obcaecati suntLorem, ipsum dolor sit amet consectetur adipisicing elit. Enim iste obcaecati suntLorem, ipsum dolor sit amet consectetur adipisicing elit. Enim iste obcaecati suntLorem, ipsum dolor sit amet consectetur adipisicing elit. Enim iste obcaecati sunt a quis minima quos aliquam fuga impedit voluptate. Aliquid fugiat dolorum rerum perspiciatis sit dolore nam sint quisquam."
+        max={20}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
